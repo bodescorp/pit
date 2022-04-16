60cc4d55a9077b3f2609f58f598b1e48389c0da4
glaykiere <glaykiere@gmail.com>
2022-04-16 00:18:54 -0300

    criadas as rotas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,22 @@
 const { Router} = require('express');
-const { listAcademic_activities, createAcademic_activities } = require('../controllers/academic_activitiesController');
-const { listUsers, createUsers } = require('../controllers/userController');
+const { listAcademic_activities, createAcademic_activities, deleteAcademic_activities } = require('../controllers/academic_activitiesController');
+const { listProfile_academic_activities } = require('../controllers/profileController');
+const { login } = require('../controllers/sessionController');
+const { listUsers, createUsers, deleteUsers } = require('../controllers/userController');
 const router = Router();
 
 
 router.get('/pit/users', listUsers);
-router.post('/pit', createUsers);
+router.post('/pit/create', createUsers);
+router.delete('/pit/delete/:id', deleteUsers);
+
+router.get('/pit/profile', listProfile_academic_activities);
+
+router.post('/pit/sessions', login);
 
 router.get('/pit/activities', listAcademic_activities);
 router.post('/pit/activities', createAcademic_activities);
+router.delete('/pit/activities/delete/:id', deleteAcademic_activities);
 
 
 module.exports = router;
