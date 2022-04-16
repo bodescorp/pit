2e13a5439046e41636c4f2b8eada85a0cb0ebace
glaykiere <glaykiere@gmail.com>
2022-04-16 00:18:23 -0300

    criada a funcao deletar usuario

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -20,10 +20,24 @@ const createUsers = async(request, response) => {
         titulacao,
         regime_trabalho
     });
+
     return response.json();
+};
+
+const deleteUsers = async(request, response) => {
+    const { id } = request.params;
+    const users_id = request.headers.authorization;
+
+    if (users_id === 'root') {
+       await connection('users').where('id', id).del();
+       return response.json({id: 'deleted'});
+    }
+
+    return response.json({error: 'erro'});
 };
 
 module.exports = {
     listUsers,
-    createUsers
+    createUsers,
+    deleteUsers
 }
