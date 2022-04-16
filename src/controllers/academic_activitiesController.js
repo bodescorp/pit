10e0e31fa68b755dc90485308e2ce39affc6c486
glaykiere <glaykiere@gmail.com>
2022-04-16 00:16:43 -0300

    criada a funcao delete e paginacao

--- src/controllers/academic_activitiesController.js
+++ src/controllers/academic_activitiesController.js
@@ -1,7 +1,23 @@
 const connection = require('../database/connection');
 
 const listAcademic_activities = async(request, response) => {
-    const activities = await connection('academic_activities').select('*');
+    const { page =1 } = request.query;
+
+    const [ count ] = await connection('academic_activities')
+        .count();
+
+    const activities = await connection('academic_activities')
+        .join('users', 'users.id', '=', 'academic_activities.users_id')
+        .limit(5)
+        .offset((page -1) * 5)
+        .select([
+            'academic_activities.*',
+            'users.nome',
+            'users.email'
+        ]);
+
+    response.header('X-Total-Count', count['count']);
+
     return response.json(activities);
 };
 
@@ -19,7 +35,25 @@ const createAcademic_activities = async(request, response) => {
     return response.json();
 };
 
+const deleteAcademic_activities = async(request, response) => {
+    const { id } = request.params;
+    const users_id = request.headers.authorization;
+
+    const activities = await connection('academic_activities')
+        .where('id', id)
+        .select('users_id')
+        .first();
+
+    if (activities.users_id === users_id) {
+       await connection('academic_activities').where('id', id).delete();
+       return response.json({activities: 'deleted'});
+    }
+
+    return response.json({error: 'erro'});
+};
+
 module.exports = {
     listAcademic_activities,
-    createAcademic_activities
+    createAcademic_activities,
+    deleteAcademic_activities
 };
